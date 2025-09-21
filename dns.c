616792f375019fbe063a26597359670391141eca
Arch me <kishudwivedi30@gmail.com>
2025-09-21 08:55:00 +0530

    getaddrinfo

--- dns.c
+++ dns.c
@@ -2,16 +2,30 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
   printf("Performing DNS lookup...\n");
 
   const char *node = "www.google.com";
-  const struct addrinfo hints;
-  int getaddrinfo(node,
-                 const char *restrict service,
-                 const struct addrinfo *restrict hints,
-                 struct addrinfo **restrict res);
+  const char *service = "http";
+  struct addrinfo *pres;
+
+  struct addrinfo hints;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;    /* Allow IPv4 or IPv6 */
+  hints.ai_socktype = SOCK_DGRAM; /* Datagram socket */
+  hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+  hints.ai_protocol = 0;          /* Any protocol */
+  hints.ai_canonname = NULL;
+  hints.ai_addr = NULL;
+  hints.ai_next = NULL;
+  int s = getaddrinfo(node, service, &hints, &pres);
+  freeaddrinfo(pres);
+  printf("%d\n", s);
+
 
   return 0;
 }
